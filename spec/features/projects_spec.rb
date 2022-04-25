c6f02aa2ddccb5e68e1569b78ad4565410a701d6
frankietorres <iamfrankietorres@gmail.com>
2022-04-25 02:17:17 +0000

    Worked on the projects_spec

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -2,19 +2,23 @@ require 'rails_helper'
 
 RSpec.feature "Projects", type: :feature do
   context "Create new project while signed in" do
+
+    # Create a test account beforehand
     before(:all) do
       visit new_user_registration_path
       fill_in "user_email", with: "test@email.com"
       fill_in "user_password", with: "password"
       fill_in "user_password_confirmation", with: "password"
       click_button "Sign up"
-
-      #visit new_project_path
-      #within("form") do
-      #  fill_in "Title", with: "Test title"
-      #end
     end
 
+    # Delete the test account after all tests are run
+    after(:all) do 
+      visit edit_user_registration_path
+      click_button "Cancel my acccount"
+    end
+
+    # Title and Description are filled in so therefore the project should be created successfully
     scenario "should be successful" do
       visit new_project_path
       fill_in "Title", with: "Test title"
@@ -23,6 +27,7 @@ RSpec.feature "Projects", type: :feature do
       expect(page).to have_content("Project was successfully created")
     end
 
+    # Description is not filled in so therefore the project creation should be unsuccessful
     scenario "should fail" do
       visit new_project_path
       fill_in "Title", with: "Test title"
@@ -31,17 +36,34 @@ RSpec.feature "Projects", type: :feature do
     end
   end
 
-  context "Create new project while logged out" do
+  # Not signed in so the new project path should redirect to a sign-in/sign-up page
+  context "Create new project while signed out" do
     scenario "should fail" do
-      click_button "Create Project"
-      expect(page).to have_content("Description can't be blank")
+      visit new_project_path
+      expect(page).to have_content("You must sign in to create projects")
     end
   end
 
-  context "Update project" do
+  context "Update project while signed in" do
     let(:project) { Project.create(title: "Test title", description: "Test content") }
+
+    # Create a test account beforehand
+    before(:all) do
+      visit new_user_registration_path
+      fill_in "user_email", with: "test@email.com"
+      fill_in "user_password", with: "password"
+      fill_in "user_password_confirmation", with: "password"
+      click_button "Sign up"
+    end
+
     before(:each) do
       visit edit_project_path(project)
+    end
+
+    # Delete the test account after all tests are run
+    after(:all) do 
+      visit edit_user_registration_path
+      click_button "Cancel my acccount"
     end
 
     scenario "should be successful" do
@@ -61,6 +83,15 @@ RSpec.feature "Projects", type: :feature do
     end
   end
 
+  # Not signed in so the edit project path should redirect to a sign-in/sign-up page
+  context "Update project while signed out" do
+    scenario "should fail" do
+      visit edit_project_path
+      expect(page).to have_content("You must sign in to edit projects")
+    end
+  end
+
+  # Deleting a project while signed in
   context "Remove existing project" do
     let!(:project) { Project.create(title: "Test title", description: "Test content") }
     scenario "remove project" do
@@ -70,4 +101,16 @@ RSpec.feature "Projects", type: :feature do
       expect(Project.count).to eq(0)
     end
   end
+
+  # Deleting a project while signed out
+  context "Remove existing project" do
+    let!(:project) { Project.create(title: "Test title", description: "Test content") }
+    scenario "remove project" do
+      visit projects_path
+      click_link "Destroy"
+      expect(page).to have_content("Project was successfully destroyed")
+      expect(Project.count).to eq(0)
+    end
+  end
+
 end
